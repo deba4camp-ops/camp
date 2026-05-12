e84d0f5f3d3925e6fda294c23f5ceda70ef7e556
camp <deba4camp@gmail.com>
2026-05-12 17:27:28 +0530

    message text

--- 1-helloworld.py
+++ 1-helloworld.py
@@ -6,3 +6,15 @@ print( type(name1))
 
 height = 5.9
 print(type(height))
+## create multiline string
+multiline_string = """This is a multiline string.
+It can span multiple lines."""
+print(f"Multiline String:\n{len(multiline_string)} characters")
+print(f"Multiline String:\n{multiline_string.index('multiline')}")
+print(f"Multiline String:\n{multiline_string.count('multilin')}")
+multiline_string = multiline_string.replace("multiline", "multi-line")
+print(f"Replace check:\n{multiline_string}")
+split_string = multiline_string.split()
+for char in split_string:
+    print(char)
+
