a385834781ca6fff77dcf5c06f2d460fa0a22ff2
camp <deba4camp@gmail.com>
2026-05-14 14:12:41 +0530

    connect with db project added

--- 4-arithmatic_operation.py
+++
@@ -1,4 +0,0 @@
-import math
-fload_val = 9.75
-ceil_val = math.ceil(fload_val)
-print(ceil_val)
--- util/global_function.py
+++
@@ -1,7 +0,0 @@
-
-def tax_calculate(products, vendors):
-    for product in products:
-        product["price"] = product["price"] + 10.00
-    for vendor in vendors:
-        vendor["location"] = vendor["location"] + " City"
-        
